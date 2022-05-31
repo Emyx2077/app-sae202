41e0679d4c300d90b7e2bc1dbfc90a355e6a8b98
emyx2077 <emily.saintetienne@gmail.com>
2022-05-31 09:24:36 +0200

    jointeamverrif

--- join_team_verif.php
+++ join_team_verif.php
@@ -8,7 +8,7 @@ $co=connexion();
 
 
 //on check si le nouveau teamCode existe
-$req = 'SELECT teamCode from teams WHERE teamCode="'.$teamCode.'";';
+$req = 'SELECT * from teams WHERE teamCode="'.$teamCode.'";';
 
 try {
     $resultat=$co->query($req); // exécuter la requête
@@ -22,7 +22,28 @@ $ligne=$resultat->rowCount();
 if ($ligne == 0){
     $_SESSION['erreur'] = "Le code de la team est invalide";
     header('location:join_team.php');
+
+}
+
+$resultat = $resultat->fetch(PDO::FETCH_ASSOC);
+
+$teamId= $resultat['teamId'];
+
+
+
+$req = 'UPDATE users SET userTeamId ="'.$teamId.'" WHERE userId="'.$_SESSION['userId'].'"';
+
+try {
+    $resultat=$co->query($req); // exécuter la requête
+} catch (PDOException $e) {
+    print "Erreur : ".$e->getMessage().'<br />';
     die();
 }
+
+header('location:board.php');
+
+
+
+
 
 
