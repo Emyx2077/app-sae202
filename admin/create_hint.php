90c29bf0773c82e42ed43dab673c400b8ba8815d
emyx2077 <emily.saintetienne@gmail.com>
2022-06-10 18:38:36 +0200

    keys

--- admin/create_hint.php
+++ admin/create_hint.php
@@ -3,20 +3,18 @@
 require'head_admin.php';
 
 $splitSentence = $_SESSION['split'];
-//unset($_SESSION['split']);
-
-//print_r($splitSentence);
 
 $size = sizeof($splitSentence);
+$_SESSION['size'] = $size;
 
 echo '<form action="create_hint_valider.php" method="POST">';
 
     for ($i = 0; $i < $size; $i++ ){
         echo 'bout de key : "'.$splitSentence[$i].'"<br>';
         echo 'Endroit<br>';
-        echo '<input type="text" name="location" /><br>';
+        echo '<input type="text" name="location'.$i.'" /><br>';
         echo 'indice (clé donné a la fin d\'une activité)<br>';
-        echo '<input type="text" name="hint" /><br><br>';
+        echo '<input type="text" name="hint'.$i.'" /><br><br>';
     }
 
     echo '<input type="submit" value="Envoyer">';
--- admin/create_hint_valider.php
+++ admin/create_hint_valider.php
@@ -1,1 +1,29 @@
 <?php
+
+require'head_admin.php';
+
+$co=connexion();
+
+$splitSentence = $_SESSION['split'];
+$size = $_SESSION['size'];
+unset($_SESSION['split, size']);
+
+
+for ($i=0 ; $i< $size; $i++) {
+    $loc = 'location'.$i;
+    $hi = 'hint'.$i;
+
+    $location = $_POST[$loc];
+    $hint = $_POST[$hi];
+    $key = $splitSentence[$i];
+
+    $req = 'INSERT INTO hash (hashKey, hashIndice, hashHint) VALUES ("'.$key.'", "'.$location.'", "'.$hint.'");';
+
+    try {
+        $resultat=$co->query($req); // exécuter la requête
+    } catch (PDOException $e) {
+        print "Erreur : ".$e->getMessage().'<br />';
+        die();
+    }
+}
+
