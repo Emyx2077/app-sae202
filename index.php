337e22a7b05e92a26b74c8afc5814061a0d7d087
emyx2077 <emily.saintetienne@gmail.com>
2022-06-13 09:51:07 +0200

    scss init

--- index.php
+++ index.php
@@ -5,28 +5,28 @@ if (!empty($_SESSION['userId'])){
 }
 ?>
 
-<div class="d-flex flex-column align-items-center">
+<div class="background d-flex flex-column align-items-center">
 
-    <div class="mt-5 form-group w-50">
+    <div class="mt-5 form-group w-50 text-white">
         <h2 class="mb-3 text-center">S'inscrire</h2>
 
         <form action="login_create.php" method="post">
             <input class="form-control" placeholder="Ton prénom" type="text" name="prenom" required/><br>
             <input class="form-control" placeholder="Ton nom" type="text" name="nom" required/><br>
             <input class="form-control" placeholder="Ton code etudiant" type="text" name="password" required/><br>
-            <input type="submit" value="S'inscrire" class="btn btn-outline-primary float-right">
+            <input type="submit" value="S'inscrire" class="btn btn-primary float-right">
         </form>
     </div>
 
 
-    <div class="mt-5 form-group w-50">
+    <div class="mt-5 form-group w-50 text-white">
         <h2 class="mb-3 text-center">Se connecter</h2>
 
         <form action="login_verif.php" method="post">
             <input class="form-control" placeholder="Ton prénom" type="text" name="prenom" required/><br>
             <input class="form-control" placeholder="Ton nom" type="text" name="nom" required/><br>
             <input class="form-control" placeholder="Ton code étudiant" type="text" name="password" required/><br>
-            <input type="submit" value="Se connecter" class="btn btn-outline-primary float-right">
+            <input type="submit" value="Se connecter" class="btn btn-primary float-right">
         </form>
 
         <?php
@@ -37,7 +37,7 @@ if (!empty($_SESSION['userId'])){
         ?>
 
     </div>
-
 </div>
+
 
 <?php require 'end.php'; ?>
