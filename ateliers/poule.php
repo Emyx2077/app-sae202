84e70ca58fbf7bf4cfed9ae9e9f10db96be22f41
emyx2077 <emily.saintetienne@gmail.com>
2022-06-18 19:45:22 +0200

    fix chemin acces

--- ateliers/poule.php
+++ ateliers/poule.php
@@ -46,7 +46,7 @@ echo '</div>';
     <div class="bg-light m-5 mt-5 p-5 border rounded mh-10" style="max-width: 350px; height : 250px;">
         <h3 class="mb-4 text-center text-black">Valider l'indice</h3>
 
-        <form action="../profil/is_valid.php" method="post">
+        <form action="profil/is_valid.php" method="post">
             <input class="form-control" placeholder="Valider un indice" type="text" name="indice" required /><br>
             <input type="submit" value="Envoyer" class="float-end btn btn-primary">
         </form>
